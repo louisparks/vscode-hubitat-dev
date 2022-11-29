a64d434784b0e2b6ee3eab722f401157ed81fb97
Louis parks <lparks@loupy.com>
2022-11-29 13:11:13 -0600

    Initial Version

---
+++ README.md
@@ -0,0 +1,45 @@
+# Hubitat Developer Tools extension README
+
+Edit your Hubitat drivers and applications in Visual Studio Code and easliy publish them to your hubitat.
+
+## Features
+
+* 1-click publishing of hubitat drivers, apps and libraries .groovy files
+* Version tracking to make sure you don't overwrite a newer version on the hub
+* Local file will auto-save when publishing
+
+The extension needs the IP/hostname of the hub.  If it's not already set in the settings, you will be prompted to enter the IP address when you first try to publish.
+When you publish for the first time, a new driver, app or library will be created on the hubitat.  The new hubitat assigned id of the code will be stored in a .hubitat/metadata.json file at the root of your workspace folder.  It shouldn't be neccessary to edit this file manually, however, if you want to associate a driver already deployed on the hubitat with a local file, you may change the id in this file to make the association.
+
+\!\[Publishing Code To The Hubitat\]\(images/hubitat_publish_new.gif\)
+
+> To publish, have the file open in the VSCode editor and type Control-Shift-P (Command-Shift-P on mac).  Start typing in Hubitat - Publish.  Click on the Hubitat - Publish option and your code will published to the hubitat.
+
+## Requirements
+
+Hubitat hub required
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `hubitat.hub.hostname`: Hostname or IP address of hubitat hub.
+* `hubitat.misc.overwriteHubitatVersion`: Set to `true` to always overwrite the version on the hubitat.
+
+## Known Issues
+Only works on hubs with "Hub Login Security" turned off.  It should be possible to do the authorization in the future, but it's not in this first release.
+## Release Notes
+
+### 1.0.0
+
+Initial release of Hubitat Developer
+
+### 1.0.1
+
+Fixed issue #.
+
+## For more information
+
+* [Hubitat Developer Documentation](https://docs2.hubitat.com/en/developer)
+
+**Enjoy!**
---
+++ src/ConfigManager.ts
@@ -0,0 +1,119 @@
+import * as fs from 'fs';
+import { TextEncoder } from 'util';
+import * as vscode from 'vscode';
+import { logger } from './Logger';
+
+const HUBITAT_CONFIG_KEY = "hubitatConfig";
+const HUB_HOSTNAME_KEY = "hubitat.hub.hostname";
+const FORCE_HUBITAT_UPDATE = "hubitat.misc.overwriteHubitatVersion";
+const CONFIG_FILENAME = "metadata.json";
+const CONFIG_DIR = ".hubitat";
+
+
+interface HubitatConfiguration {
+  files: Array<HubitatCodeFile>
+}
+
+export enum CodeType {
+  driver = "driver",
+  app = "app",
+  library = "library"
+}
+
+export interface HubitatCodeFile {
+  filepath?: string;
+  codeType?: CodeType;
+  id?: number;
+  version?: number;
+  source?: string;
+}
+
+export class HubitatConfigManager {
+  context: vscode.ExtensionContext;
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
+
+  async load(workspaceFolder: vscode.Uri): Promise<HubitatConfiguration> {
+    let workspaceFolderConfigFile = vscode.Uri.parse(workspaceFolder + '/' + CONFIG_DIR + '/' + CONFIG_FILENAME);
+    if (!fs.existsSync(workspaceFolderConfigFile.fsPath)) {
+      logger.info(`creating new config file at [${workspaceFolderConfigFile}]`);
+      return this.saveConfigToWorkspaceFolder(workspaceFolder, { files: [] });
+    }
+    const workspaceConfig = await vscode.workspace.fs.readFile(vscode.Uri.parse(workspaceFolder + '/' + CONFIG_DIR + '/' + CONFIG_FILENAME));
+    const config = JSON.parse(String(workspaceConfig)) as HubitatConfiguration;
+    return config;
+  }
+
+  async lookupCodeFile(filepath: string): Promise<HubitatCodeFile | undefined> {
+    let workspaceFolder = this.findWorkspaceConfigFolderFileForPath(filepath);
+    let config = await this.load(workspaceFolder);
+    let file = config.files.find(file => file.filepath === filepath);
+    return file;
+  }
+
+  async saveCodeFile(codeFile: HubitatCodeFile) {
+    delete codeFile.source;
+    let codeFilePath = vscode.Uri.parse(codeFile.filepath!);
+    const workspaceFolder = vscode.workspace.getWorkspaceFolder(codeFilePath);
+    let config = await this.load(workspaceFolder?.uri!);
+    let existingIndex = config.files.findIndex(f => f.filepath === codeFile.filepath);
+    if (existingIndex > -1) {
+      config.files[existingIndex] = codeFile;
+    } else {
+      config.files.push(codeFile);
+    }
+    await this.saveConfigToWorkspaceFolder(workspaceFolder?.uri!, config);
+  }
+
+  determineCodeType(filepath: string): Promise<CodeType | undefined> {
+    return this.determineCodeTypeByContent(filepath);
+  }
+
+  async determineCodeTypeByContent(filepath: string): Promise<CodeType | undefined> {
+    const driverMatchEx = /^\s*metadata\s*{/gm;
+    const appMatchEx = /^\s*definition\s*\(/mg;
+    const libMatchEx = /^\s*library\s*\(/mg;
+
+    const sourceText = (await vscode.workspace.openTextDocument(filepath)).getText();
+    if (driverMatchEx.exec(sourceText)) {
+      return CodeType.driver;
+    }
+    else if (appMatchEx.exec(sourceText)) {
+      return CodeType.app;
+    }
+    else if (libMatchEx.exec(sourceText)) {
+      return CodeType.library;
+    }
+    return undefined;
+  }
+
+  getActiveHub(): string | undefined {
+    const host = vscode.workspace.getConfiguration().get(HUB_HOSTNAME_KEY) as string;
+    return host;
+  }
+
+  setActiveHub(hostname: string) {
+    vscode.workspace.getConfiguration().update(HUB_HOSTNAME_KEY, hostname, vscode.ConfigurationTarget.Global);
+  }
+
+  forceHubitatUpdate(): boolean {
+    return vscode.workspace.getConfiguration().get(FORCE_HUBITAT_UPDATE) as boolean;
+  }
+
+  clear() {
+    this.context.workspaceState.update(HUBITAT_CONFIG_KEY, "");
+    vscode.workspace.getConfiguration().update(HUB_HOSTNAME_KEY, null, vscode.ConfigurationTarget.Global);
+  }
+
+  async saveConfigToWorkspaceFolder(workspaceFolder: vscode.Uri, config: HubitatConfiguration): Promise<HubitatConfiguration> {
+    const folder = vscode.workspace.getWorkspaceFolder(workspaceFolder);
+    await vscode.workspace.fs.writeFile(vscode.Uri.parse(folder?.uri.path + '/' + CONFIG_DIR + '/' + CONFIG_FILENAME), new TextEncoder().encode(JSON.stringify(config, null, 2)));
+    return config;
+  }
+
+  findWorkspaceConfigFolderFileForPath(filepath: string): vscode.Uri {
+    return vscode.workspace.getWorkspaceFolder(vscode.Uri.parse(filepath))?.uri!;
+  }
+
+}
