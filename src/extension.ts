74815d2712847321a1b74bc9b5f1e9748099de95
Louis parks <lparks@loupy.com>
2022-11-30 16:04:58 -0600

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -3,65 +3,114 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { HubitatConfigManager } from './config';
 import { HubitatClient, PublishStatus } from './hubitat';
+
+let myStatusBarItem: vscode.StatusBarItem;
+let configManager: HubitatConfigManager;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "hubitat-dev" is now active!');
+  console.log('Congratulations, your extension "hubitat-dev" is now active!');
 
-	let disposable = vscode.commands.registerCommand('hubitat-dev.publish', (uri: vscode.Uri) => {
-		publish(context);
-	});
-	let disposable2 = vscode.commands.registerCommand('hubitat-dev.clearConfig', (uri: vscode.Uri) => {
-		context.workspaceState.update("hubitatHostname", null);
-	});
+  let disposable = vscode.commands.registerCommand('hubitat-dev.publish', (uri: vscode.Uri) => {
+    publish(context);
+  });
+  let disposable2 = vscode.commands.registerCommand('hubitat-dev.clearConfig', (uri: vscode.Uri) => {
+    context.workspaceState.update("hubitatHostname", null);
+  });
 
-	context.subscriptions.push(disposable, disposable2);
+  context.subscriptions.push(disposable, disposable2);
+
+  const myCommandId = 'sample.showSelectionCount';
+  context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+    publish(context);
+  }));
+  configManager = new HubitatConfigManager(context);
+
+  //status bar
+  myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+  myStatusBarItem.command = myCommandId;
+  context.subscriptions.push(myStatusBarItem);
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+  updateStatusBarItem();
+}
+
+function updateStatusBarItem(): void {
+  const codeFile = configManager.lookupCodeFile(vscode.window.activeTextEditor?.document.uri.path!);
+  myStatusBarItem.text = `$(home) Hubitat id ${codeFile?.id}`;
+  myStatusBarItem.show();
 }
 
 async function publish(context: vscode.ExtensionContext) {
-	const document = vscode.window.activeTextEditor?.document;
+  const document = vscode.window.activeTextEditor?.document;
 
-	let host = context.workspaceState.get("hubitatHostname") as string | null;
-	if (!host) {
-		host = await promptUserForHubitatHostname();
-		if (host) {
-			context.workspaceState.update("hubitatHostname", host);
-		}
-	}
+  let host = context.workspaceState.get("hubitatHostname") as string | null;
+  if (!host) {
+    host = await promptUserForHubitatHostname();
+    if (host) {
+      context.workspaceState.update("hubitatHostname", host);
+    }
+  }
 
-	if (document?.isDirty) {
-		await document.save();
-		console.log(`file saved ${!document?.isDirty}`);
-	}
+  if (document?.isDirty) {
+    await document.save();
+  }
+  let configManager = new HubitatConfigManager(context);
 
-	const documentPath = document?.uri.path!;
+  const documentPath = document?.uri.path!;
 
-	const message = vscode.window.showInformationMessage(`Publishing to hubitat file  ${documentPath}`);
-	const hubitat = new HubitatClient(host!);
-	const result = await hubitat.publish(833, documentPath);
-	if (result.status === PublishStatus.success) {
-		vscode.window.showInformationMessage(`Success ${documentPath}`);
-	} else {
-		vscode.window.showErrorMessage(`Failed to publich to hubitat ${document?.uri.path}`);
-	}
+  const message = vscode.window.showInformationMessage(`publishing file to hubitat [${documentPath}]`);
+  const hubitat = new HubitatClient(host!);
+  let codeFile = configManager.lookupCodeFile(documentPath);
+  if (!codeFile?.id) {
+    const id = await promptUserForFileId();
+    codeFile!.id = Number(id);
+  }
+
+
+  const result = await hubitat.publish(codeFile!);
+  if (result.status === PublishStatus.success) {
+    const header = `Success ${documentPath}`;
+    //const options: vscode.MessageOptions = { detail: 'Message Description', modal: true };
+    const selection = await vscode.window.showInformationMessage(header);
+    configManager.saveCodeFile(result.codeFile);
+  } else {
+    vscode.window.showErrorMessage(`Failed to publich to hubitat ${document?.uri.path}`);
+  }
 
 }
 
 async function promptUserForHubitatHostname(): Promise<string | null> {
-	const hubitatHostInput = await vscode.window.showInputBox({
-		title: "Hubitat IP Address",
-		placeHolder: "IP address or hostname (ex.. 192.168.1.23)",
-		prompt: "Please enter Hubitat host or IP address",
-	});
-	if (!hubitatHostInput) {
-		vscode.window.showErrorMessage('Hubitat address required, action aborted');
-		return null;
-	} else {
-		return hubitatHostInput;
-	}
+  const hubitatHostInput = await vscode.window.showInputBox({
+    title: "Hubitat IP Address",
+    placeHolder: "IP address or hostname (ex.. 192.168.1.23)",
+    prompt: "Please enter Hubitat host or IP address",
+  });
+  if (!hubitatHostInput) {
+    vscode.window.showErrorMessage('Hubitat address required, action aborted');
+    return null;
+  } else {
+    return hubitatHostInput;
+  }
 }
+async function promptUserForFileId(): Promise<number | null> {
+  const idInput = await vscode.window.showInputBox({
+    title: "Source Code ID",
+    placeHolder: "ID assigned to the code file",
+    prompt: "Please enter the id of the codefile",
+    ignoreFocusOut: true
+  });
+  if (!idInput || isNaN(Number(idInput))) {
+    vscode.window.showErrorMessage('file id not valid, action aborted');
+    return null;
+  } else {
+    return Number(idInput);
+  }
+}
+
+
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
--- src/hubitat.ts
+++ src/hubitat.ts
@@ -2,8 +2,7 @@
 import * as rm from 'typed-rest-client/RestClient';
 import * as hm from 'typed-rest-client/HttpClient';
 import * as vscode from 'vscode';
-
-const hubitatHost = "192.168.125.20";
+import { CodeType, HubitatCodeFile } from './config';
 
 interface CodeResponse {
     id: number;
@@ -22,6 +21,8 @@ export enum PublishStatus { success, failure };
 
 export interface PublishResult {
     status: PublishStatus;
+    errorMessage: string | undefined;
+    codeFile: HubitatCodeFile;
 }
 
 export class HubitatClient {
@@ -37,30 +38,60 @@ export class HubitatClient {
                 "Accept": 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            socketTimeout: 5000
+            socketTimeout: 10000,
+            allowRedirects: false
         });
     }
 
-    async publish(id: number, filePath: string): Promise<PublishResult> {
-        console.log("publishing ", filePath, " to ", this.hostname);
+    async publish(codeFile: HubitatCodeFile): Promise<PublishResult> {
+        console.log(`publishing ${codeFile.filepath} to ${this.hostname}`);
         try {
-            let infoRes: rm.IRestResponse<CodeResponse> = await this.rest.get<CodeResponse>(`/driver/ajax/code`, { queryParameters: { params: { "id": id } } });
-            const sourceText = (await vscode.workspace.openTextDocument(filePath)).getText();
-            let body: any = { id: id, version: infoRes.result?.version, source: sourceText };
+            try {
+                let infoRes: rm.IRestResponse<CodeResponse> = await this.rest.get<CodeResponse>(`/${codeFile.codeType}/ajax/code`, { queryParameters: { params: { "id": codeFile.id } } });
+                codeFile.version = infoRes.result?.version as number;
+            }
+            catch (error: any) {
+                if (error['statusCode'] === 500) {
+                    //existing id was not found, creating a new one
+                    codeFile = await this.createNewId(codeFile);
+                }
 
-            const form = Object.keys(body).map(key => {
-                return `${key}=${encodeURIComponent(body[key])}`;
-            }).join("&");
+            }
 
-            let updateResponse = await this.http.post(`http://${this.hostname}/driver/ajax/update`, form);
-            console.log(updateResponse.message.statusCode);
-            const result = JSON.parse(await updateResponse.readBody());
-            return { status: result.status === 'success' ? PublishStatus.success : PublishStatus.failure };
+            const sourceText = (await vscode.workspace.openTextDocument(codeFile.filepath)).getText();
+
+            const form = this.createForm({ id: codeFile.id, version: codeFile.version, source: sourceText });
+
+            let updateResponse = await this.http.post(`http://${this.hostname}/${codeFile.codeType}/ajax/update`, form);
+            const result = JSON.parse(await updateResponse.readBody()) as UpdateResponse;
+            codeFile.version = codeFile.version + 1;
+            return { status: result.status === 'success' ? PublishStatus.success : PublishStatus.failure, codeFile, errorMessage: undefined };
         }
-        catch (e) {
-            console.log("error", e);
-            return { status: PublishStatus.failure };
+        catch (error) {
+            let message = 'Unknown Error';
+            if (error instanceof Error) {
+                message = error.message;
+            }
+            console.log("error", error);
+            return { status: PublishStatus.failure, codeFile, errorMessage: message };
         }
+    }
+    async createNewId(codeFile: HubitatCodeFile): Promise<HubitatCodeFile> {
+        const sourceText = (await vscode.workspace.openTextDocument(codeFile.filepath)).getText();
+        const body = this.createForm({ id: '', version: '', create: '', source: sourceText });
+        let updateResponse = await this.http.post(`http://${this.hostname}/${codeFile.codeType}/save`, body, { Authorization: "Basic YWRtaW46YWRtaW4=" });
+        console.log(updateResponse.message.statusCode, updateResponse.message.statusMessage, updateResponse.message.headers.location);
+        const newId = updateResponse.message.headers.location?.split("/").pop();
+        codeFile.id = Number(newId);
+        codeFile.version = 1;
+        return codeFile;
+    }
+
+    createForm(body: any): string {
+        const form = Object.keys(body).map(key => {
+            return `${key}=${encodeURIComponent(body[key])}`;
+        }).join("&");
+        return form;
     }
 }
 
